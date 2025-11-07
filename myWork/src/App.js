c53b8c62f7a16c58d8bed84dc1fdea0486fdf2da
nikhilshaji17 <nikhilshaji17@gmail.com>
2025-11-07 09:52:03 +0400

    Just changed spaces to tabs

--- myWork/src/App.js
+++ myWork/src/App.js
@@ -1,29 +1,29 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
 const Pizza = (props) => {
-  return React.createElement("div", null, [
-    React.createElement("h1", null, props.name),
-    React.createElement("p", null, props.description),
-  ]);
+	return React.createElement("div", null, [
+		React.createElement("h1", null, props.name),
+		React.createElement("p", null, props.description),
+	]);
 };
 
 const App = () => {
-  return React.createElement("div", null, [
-    React.createElement("h1", null, "Padre Gino's"),
-    Pizza({
-      name: "The Pepperoni Pizza",
-      description: "Mozzarella and Pepperoni",
-    }),
-    React.createElement(Pizza, {
-      name: "The Hawaiian Pizza",
-      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
-    }),
-    React.createElement(Pizza, {
-      name: "The Big Meat Pizza",
-      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
-    }),
-  ]);
+	return React.createElement("div", null, [
+		React.createElement("h1", null, "Padre Gino's"),
+		Pizza({
+			name: "The Pepperoni Pizza",
+			description: "Mozzarella and Pepperoni",
+		}),
+		React.createElement(Pizza, {
+			name: "The Hawaiian Pizza",
+			description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+		}),
+		React.createElement(Pizza, {
+			name: "The Big Meat Pizza",
+			description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+		}),
+	]);
 };
 
 const container = document.getElementById("root"); // Rendering depends on where your root starts.

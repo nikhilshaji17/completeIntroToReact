ca85bfb8dab6ba1df3f17dc2b1eadc513af87a83
nikhilshaji17 <nikhilshaji17@gmail.com>
2025-11-07 16:29:54 +0400

    Working on implementing hooks

--- myWork/src/App.jsx
+++ myWork/src/App.jsx
@@ -1,27 +1,8 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
-  return (
-    <div>
-      <h1>Padre Gino's - Order Now</h1>
-      <Pizza
-        name="Pepperoni"
-        description="pep, cheese n stuff"
-        image={"/public/pizzas/pepperoni.webp"}
-      />
-      <Pizza
-        name="Hawaiian"
-        description="ham, pineapple n stuff"
-        image={"/public/pizzas/hawaiian.webp"}
-      />
-      <Pizza
-        name="Americano"
-        description="french fries, hot dogs n stuff"
-        image={"/public/pizzas/big_meat.webp"}
-      />
-    </div>
-  );
+  return <Order />;
 };
 
 const container = document.getElementById("root"); // Rendering depends on where your root starts.

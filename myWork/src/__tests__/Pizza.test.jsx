9cea827daf771028b0bdb7587dbd145cc929c390
nikhilshaji17 <nikhilshaji17@gmail.com>
2025-11-13 10:51:01 +0400

    Added mock api tests

--- myWork/src/__tests__/Pizza.test.jsx
+++ myWork/src/__tests__/Pizza.test.jsx
@@ -1,15 +1,24 @@
-import { render } from "@testing-library/react";
-import { expect, test } from "vitest";
-import  Pizza  from "../Pizza";
+import { render, cleanup } from "@testing-library/react";
+import { expect, test, afterEach } from "vitest";
+import Pizza from "../Pizza";
 
-test("alt text renders on Pizza image", async () => {
+afterEach(cleanup);
+
+test("alt text renders on Pizza image", () => {
   const name = "My Favorite Pizza";
   const src = "https://picsum.photos/200";
   const screen = render(
-    <Pizza name={name} description="a pizza" image={src} />
+    <Pizza name={name} description="a pizza" image={src} />,
   );
 
   const img = screen.getByRole("img");
   expect(img.src).toBe(src);
   expect(img.alt).toBe(name);
+});
+
+test("to have default image if none is provided", () => {
+  const screen = render(<Pizza name="default name" description="a pizza" />);
+
+  const img = screen.getByRole("img");
+  expect(img.src).not.toBe("");
 });
---
+++ myWork/src/__tests__/contact.lazy.test.jsx
@@ -0,0 +1,52 @@
+import { render } from "@testing-library/react";
+import { expect, test, vi } from "vitest";
+import createFetchMock from "vitest-fetch-mock";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { Route } from "../routes/contact.lazy";
+import { ScrollRestoration } from "@tanstack/react-router";
+
+const queryClient = new QueryClient();
+
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks(); // This allows us to fake calls of the fetch() function
+
+test("can submit contact form", async () => {
+  fetchMocker.mockResponse(JSON.stringify({ status: "ok" })); // This is the response that will be sent back
+
+  const screen = render(
+    <QueryClientProvider client={queryClient}>
+      <Route.options.component />
+    </QueryClientProvider>,
+  );
+
+  const nameInput = screen.getByPlaceholderText("Name");
+  const emailInput = screen.getByPlaceholderText("Email");
+  const msgTextArea = screen.getByPlaceholderText("Message");
+
+  const testData = {
+    name: "Brian",
+    email: "brian@gmail.com",
+    message: "Hello, testing the contact form",
+  };
+
+  nameInput.value = testData.name;
+  emailInput.value = testData.email;
+  msgTextArea.value = testData.message;
+
+  const btn = screen.getByRole("button");
+  btn.click();
+
+  const h3 = await screen.findByRole("heading", { level: 3 });
+  expect(h3.innerText).toContain("Submitted!");
+
+  const requests = fetchMocker.requests();
+  expect(requests.length).toBe(1);
+  expect(requests[0].url).toBe("/api/contact");
+  expect(fetchMocker).toHaveBeenCalledWith("/api/contact", {
+    body: JSON.stringify(testData),
+    headers: {
+      "Content-Type" : "application/json"
+    },
+    method: "POST",
+  });
+});
